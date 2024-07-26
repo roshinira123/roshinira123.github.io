17796799bb3faf2e99f4ce07ef6fcdd96dccfdcf
roshinira123 <roshinila@gmail.com>
2024-07-25 21:43:32 -0700

    Remove ReadMe

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Roshini!
+            Hi, I am Roshini!
             <br className="hidden lg:inline-block" />I am a{" "}
             <span>
               <Typewriter
@@ -31,7 +31,7 @@ export default function About() {
             </span>
           </h1>
           <p className="mb-8 leading-relaxed">
-            I'm a Computer Science with Business Applications major at UC
+            I am a Computer Science with Business Applications major at UC
             Riverside! I am highly motivated, self-driven with strong leadership
             qualities, and looking for challenging and innovative roles in tech,
             which will enable me to utilize and grow my skills.
